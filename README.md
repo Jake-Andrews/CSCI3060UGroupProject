03ff870990e43db78a9fcfb0be5893db47b39b6a
Jake-Andrews <jake.andrews1@ontariotechu.net>
2022-03-06 20:50:43 -0500

    Added a basic readme

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # CSCI3060UGroupProject
+The program is run, by running the Main.java file. The code was written in vscode and the csci3060ugroupproject folder is open
+in the code editor, so the current directory is the csci3060ugroupproject folder and the program will be able to find the .txt files. 
+The program will ask for the user to type "login". Once they do, it will ask for their username. 
+When a correct username is given, the program will ask for a command will accept the required commands and a help command
+that lists the various commands. 
+
+All of the input and output files are in the root directory, "availablerentalsfile.txt, dailytransactionsfile.txt, useraccounts.txt"
--- module/src/main/Main.java
+++ module/src/main/Main.java
@@ -1,7 +1,16 @@
 package main;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
+/*
+CSCI3060UGroupProject
+The program is run, by running the Main.java file. The code was written in vscode and the csci3060ugroupproject folder is open
+in the code editor, so the current directory is the csci3060ugroupproject folder and the program will be able to find the .txt files. 
+The program will ask for the user to type "login". Once they do, it will ask for their username. 
+When a correct username is given, the program will ask for a command will accept the required commands and a help command
+that lists the various commands. 
 
+All of the input and output files are in the root directory, "availablerentalsfile.txt, dailytransactionsfile.txt, useraccounts.txt"
+*/
 public class Main {
         //one large while loop to constantly accept input and commands from the user.
         //gets user to type login, then type their username, validates that the usernameis in the useraccounts.txt file
