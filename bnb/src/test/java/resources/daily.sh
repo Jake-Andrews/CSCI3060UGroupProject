1fbe2143a1107f4acb57629eeb71cb7c1401533a
Jake-Andrews <jake.andrews1@ontariotechu.net>
2022-04-23 23:07:18 -0400

    Phase 5 daily bash script without backend stub

--- bnb/src/test/java/resources/daily.sh
+++ bnb/src/test/java/resources/daily.sh
@@ -1,8 +1,11 @@
-#!/bin/bash
-#Getting the path where the bash script is located irrespective of your current directory
 parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
-cd "$parent_path" #change directory to the scripts directory
+cd "$parent_path" #cd into transaction_sessions
+
+#Deletes transactions file in the directory
 find -type f -name '*dailytransactionsfile*' -delete #delete any file that matches the string provided
+#Remove all the files in merged_transaction_files
+rm $parent_path/merged_transaction_files
+
 
 #overwriting useracounts.txt file, so we have the same starting state for every run of tests 
 echo    "UUUUUUUUUU_____AA" > useraccounts.txt #> overwrites
@@ -35,8 +38,24 @@ echo    "a943c2b1_USER___________Toronto___________________3_400.00_f_00" >> ava
 echo    "a943c2b2_USER___________Toronto___________________3_400.00_f_00" >> availablerentalsfile.txt 
 echo -n "a943c2b3_USER___________Toronto___________________3_400.00_f_00" >> availablerentalsfile.txt #-n to remove trailing new line
 
-#loop through all the files in the inputs folder
-for file in $parent_path/inputs/*; do 
+#Run the program on all of the input files in "inputs_transactions"
+for file in $parent_path/inputs_transactions/*; do 
     echo "running test ${file##*/}" 
-    java -jar $parent_path/../../../../target/bnb-1.0-SNAPSHOT-jar-with-dependencies.jar availablerentalsfile.txt useraccounts.txt < $file > $parent_path/outputs/${file##*/} #${file##*/} contains the filename
+    java -jar $parent_path/../../../../target/bnb-1.0-SNAPSHOT-jar-with-dependencies.jar availablerentalsfile.txt useraccounts.txt < $file  #${file##*/} contains the filename
 done
+
+#Moving the transaction files into merged_transaction_files
+for file in *; do
+    if [[ ${file##*/} =  "dailytransactionsfile"* ]]; then
+        echo "Transaction File: ${file##*/}"
+        mv $file $PWD/merged_transaction_files
+    else
+        echo "Normal File: ${file##*/}"
+    fi
+done
+
+#cd into folder containing the transaction files, then 
+#Concatenate the daily transaction txt files into a merged txt file
+cd $PWD/merged_transaction_files
+cat *.txt >> mergedtransactionsfile.txt
+find -type f -name '*dailytransactionsfile*' -delete #delete any file that matches the string provided
